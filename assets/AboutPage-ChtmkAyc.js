7e272417f0f9d439cc0384c33675e9cd0f2c468b
Jonathan Lowell <elderlowell@gmail.com>
2024-03-21 02:54:46 -0600

    Updates

---
+++ assets/AboutPage-ChtmkAyc.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./NavView.vue_vue_type_script_setup_true_lang-BAgbQyBt.js";import{d as a,c as t,w as o,o as s,a as _}from"./index-fsOL1KMH.js";const c=_("div",{class:""},"About",-1),u=a({__name:"AboutPage",setup(n){return(r,i)=>(s(),t(e,null,{default:o(()=>[c]),_:1}))}});export{u as default};
---
+++ assets/WidgetsPage-Cfmp_e22.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./NavView.vue_vue_type_script_setup_true_lang-BAgbQyBt.js";import{d as s,c as a,w as o,o as c,a as e}from"./index-fsOL1KMH.js";const _=e("div",{class:""},"Widgets",-1),n=e("embed",{src:"http://localhost:4173/widgets/timer",type:""},null,-1),p=s({__name:"WidgetsPage",setup(r){return(i,d)=>(c(),a(t,null,{default:o(()=>[_,n]),_:1}))}});export{p as default};
